78ecf498bd74b07d8658b5a79858aaa219f674d7
Jungsik Choi <108637948+cjs0212@users.noreply.github.com>
2022-11-28 19:04:02 +0900

    0.0.1

--- app/src/main/java/com/example/testlib/MainActivity.java
+++ app/src/main/java/com/example/testlib/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Librarytest.showToast(getApplicationContext() , "123");
+        Librarytest.showToast(getApplicationContext() , "12");
 
 
     }
